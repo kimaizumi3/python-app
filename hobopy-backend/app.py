22f2cc161cfefbea7f7a8f692fc88a7a41d678a2
kimaizumi3 <osyamojikz2003@gmail.com>
2022-04-22 18:38:10 +0900

    フロントエンドまで

--- hobopy-backend/app.py
+++ hobopy-backend/app.py
@@ -1,12 +1,27 @@
-from chalice import Chalice
+from chalice import Chalice, NotFoundError
+from chalicelib import database
 
 app = Chalice(app_name='hobopy-backend')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+# @app.route('/')
+# def index():
+#     return {'hello': 'world'}
 
+# すべてのTodoを取得する
+@app.route('/todos', methods=['GET'])
+def get_all_todos():
+    return database.get_all_todos()
+
+
+# 指定されたIDのToDoを取得する 無ければ　エラーをかえす
+@app.route('/todos/{todo_id}', methods=['GET'])
+def get_todo(todo_id):
+    todo = database.get_todo(todo_id)
+    if todo:
+        return todo
+    else:
+        raise NotFoundError('Todo not found.')   
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
--- hobopy-backend/chalicelib/database.py
+++ hobopy-backend/chalicelib/database.py
@@ -1,20 +1,20 @@
 # 疑似database
-BLUE_ THREE = [
+BLUE_THREE = [
      { 
-         'id': 'L 5',
+         'id': 'L5',
          'title': '夢 の 舞台 へ 駆け 上がる', 
          'memo': 'TONOSAKI', 
          'priority': 3, 'completed': False, 
      }, 
      { 
-         'id': 'L 6',
+         'id': 'L6',
          'title': '今 ここ で 魅せる', 
          'memo': 'GENDA', 
          'priority': 2, 
          'completed': False, 
      }, 
      { 
-         'id': 'L 8', 
+         'id': 'L8', 
          'title': 'その 瞬間 を 掴む', 
          'memo': 'KANEKO', 
          'priority': 1, 
@@ -23,14 +23,14 @@ BLUE_ THREE = [
 ]
 
 # すべてのレコードを取得
-def get all_todos():
-    return BLUE_THERE
+def get_all_todos():
+    return BLUE_THREE
 
 # 指定されたIDのレコードを取得
 def get_todo(todo_id):
-    for todo in BLUE_THERE:
+    for todo in BLUE_THREE:
         if todo ['id'] == todo_id:
             return todo
-    return none
+    return None
 
 
