d8f2e80b220fab0120b12c532b52c42d50e60194
Kazuma.Imaizumi <osyamojikz2003@gmail.com>
2022-05-01 21:39:41 +0900

    miss

--- hobopy-backend/app.py
+++ hobopy-backend/app.py
@@ -3,27 +3,22 @@ from chalicelib import database
 
 app = Chalice(app_name='hobopy-backend')
 
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-# すべてのTodoを取得する
-@app.route('/todos', methods=['GET'])
+# すべてのToDoを取得する
+@app.route('/todos', methods=['GET'], cors=True)
 def get_all_todos():
     return database.get_all_todos()
 
-
-# 指定されたIDのToDoを取得する 無ければ　エラーをかえす
-@app.route('/todos/{todo_id}', methods=['GET'])
+# 指定されたIDのToDoを取得する
+@app.route('/todos/{todo_id}', methods=['GET'], cors=True)
 def get_todo(todo_id):
     todo = database.get_todo(todo_id)
     if todo:
         return todo
     else:
-        raise NotFoundError('Todo not found.')   
+        raise NotFoundError('Todo not found.')  # 404を返す
 
-@app.route('/todos', methods=['POST'])
+# Todoを登録する
+@app.route('/todos', methods=['POST'], cors=True)
 def create_todo():
     # リクエストメッセージボディを取得する
     todo = app.current_request.json_body
@@ -33,37 +28,19 @@ def create_todo():
         if key not in todo:
             raise BadRequestError(f"{key} is required.")
 
+    # データを登録する
     return database.create_todo(todo)
 
-@app.route('/todos/{todo_id}', methods=['PUT'])
+# 指定されたIDのToDoを更新する
+@app.route('/todos/{todo_id}', methods=['PUT'], cors=True)
 def update_todo(todo_id):
-    # リクエストメッセージボディを取得する
     changes = app.current_request.json_body
 
     # データを更新する
     return database.update_todo(todo_id, changes)
 
-@app.route('/todos/{todo_id}', methods=['DELETE'])
+# 指定されたIDのToDoを削除する
+@app.route('/todos/{todo_id}', methods=['DELETE'], cors=True)
 def delete_todo(todo_id):
-    # データを削除
+    # データを削除する
     return database.delete_todo(todo_id)
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
--- hobopy-backend/chalicelib/database.py
+++ hobopy-backend/chalicelib/database.py
@@ -1,34 +1,40 @@
 import os
 import uuid
-import boto3 
+import boto3
 from boto3.dynamodb.conditions import Key
 
- # ① DynamoDB への 接続 を 取得 する 
-def _get_database(): 
-    endpoint = os.environ.get("DB_ENDPOINT") 
+# DynamoDBへの接続を取得する
+def _get_database():
+    endpoint = os.environ.get('DB_ENDPOINT')
     if endpoint:
-        return boto3.resource("dynamodb", endpoint_url = endpoint)
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/resources.html
+        return boto3.resource('dynamodb', endpoint_url=endpoint)
     else:
-        return boto3.resource("dynamodb")
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/resources.html
+        return boto3.resource('dynamodb')
 
-# ② すべて の レコード を 取得 する 
+# すべてのレコードを取得する
 def get_all_todos():
-    table = _get_database().Table(os.environ["DB_TABLE_NAME"])
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#using-an-existing-table
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#querying-and-scanning
     response = table.scan()
-    return response["Items"]
+    return response['Items']
 
-# ③指定されたID の レコード を 取得 する 
+# 指定されたIDのレコードを取得する
 def get_todo(todo_id):
-    table = _get_database().Table(os.environ["DB_TABLE_NAME"])
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#using-an-existing-table
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#querying-and-scanning
     response = table.query(
-        KeyConditionExpression = Key("id").eq(todo_id)
+        KeyConditionExpression=Key('id').eq(todo_id)
     )
-    items = response["Items"]
-    return items[ 0] if items else None
+    items = response['Items']
+    return items[0] if items else None
 
-# TODOを登録する
+# レコードを登録する
 def create_todo(todo):
-    # 登録内容の作成
+    # 登録内容を作成する
     item = {
         'id': uuid.uuid4().hex,
         'title': todo['title'],
@@ -36,35 +42,46 @@ def create_todo(todo):
         'priority': todo['priority'],
         'completed': False,
     }
-    # DynamoDBにデータを登録
+
+    # DynamoDBにデータを登録する
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#using-an-existing-table
     table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#creating-a-new-item
     table.put_item(Item=item)
     return item
 
-# データ更新
+# 指定されたIDのレコードを更新する
 def update_todo(todo_id, changes):
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#using-an-existing-table
     table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+
+    # クエリを構築する
     update_expression = []
     expression_attribute_values = {}
     for key in ['title', 'memo', 'priority', 'completed']:
         if key in changes:
-            # :={key[0:1]}は空白入れない。
             update_expression.append(f"{key} = :{key[0:1]}")
             expression_attribute_values[f":{key[0:1]}"] = changes[key]
+
+    # DynamoDBのデータを更新する
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#updating-item
     result = table.update_item(
-        Key = {
+        Key={
             'id': todo_id,
-            },
-            UpdateExpression = 'set  ' + ','.join(update_expression),
-            ExpressionAttributeValues = expression_attribute_values,
-            ReturnValues = 'ALL_NEW'
+        },
+        UpdateExpression='set ' + ','.join(update_expression),
+        ExpressionAttributeValues=expression_attribute_values,
+        ReturnValues='ALL_NEW'
     )
     return result['Attributes']
 
+# 指定されたIDのレコードを削除する
 def delete_todo(todo_id):
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#using-an-existing-table
     table = _get_database().Table(os.environ['DB_TABLE_NAME'])
 
-    # DynamoDBのデータを削除
+    # DynamoDBのデータを削除する
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html#deleting-item
     result = table.delete_item(
         Key={
             'id': todo_id,
@@ -72,4 +89,3 @@ def delete_todo(todo_id):
         ReturnValues='ALL_OLD'
     )
     return result['Attributes']
-
